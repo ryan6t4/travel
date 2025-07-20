d4db0a70f153eef08d36700cbd0d9b205a53a6cd
ryan6t4 <mdrayhanh585@gmail.com>
2025-07-20 17:42:22 +0600

    bug fixed

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const Listing = require('./models/listing');
-const path = require('path');   
+const mongoose = require("mongoose");
+const Listing = require("./models/listing");
+const path = require("path");
 
-main().then(()=> {
-    console.log('connected to mongodb');
-}).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-})
+main()
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/travel',
+  await mongoose.connect("mongodb://localhost:27017/travel");
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res )=> {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.get("/listings", async (res, req) => {
-   const allListings = await Listing.find({});
-    res.render("index.ejs", {allListings});
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
-
-
-
-
-
-
 
 app.listen(8080, () => {
-    console.log ('connected to port 8080');
+  console.log("connected to port 8080");
 });
